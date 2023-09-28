4d5f8cec50c23d90d6f4c7132d0a0231b08aa7b0
Akash tyagi <akashtyagi4ui@gmail.com>
2023-09-28 13:39:04 +0530

    this is initial commit

---
+++ src/main/java/com/autoDeployement/Controllers/firstController.java
@@ -0,0 +1,16 @@
+package com.autoDeployement.Controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class firstController {
+
+    @GetMapping("/view")
+    public ResponseEntity<String> view(){
+        return ResponseEntity.ok("I am View Page");
+    }
+}
---
+++ src/test/java/com/autoDeployement/AutoDeployementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoDeployement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDeployementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
